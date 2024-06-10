c905424ddbc936b5fdc0f9669a50ee225ce3fccd
Jsckp <168212490+Jsckp@users.noreply.github.com>
2024-06-10 16:13:27 -0500

    funciones.h

---
+++ funciones.h
@@ -0,0 +1,4 @@
+void imprimirAlumnos(char alumnos[][2][20],double notas[][3]);
+void buscarAlumnoXApellido(char alumnos[][2][20], double notas[][3]);
+void editarCalificaciones(char alumnos[][2][20], double notas[][3]);
+int buscarPersonaXCedula(char personas[][4][20],char cedula[20]);
